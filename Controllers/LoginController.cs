95ecfa5a438d6df35a6eecdc5e68b01e4587580f
Randi Firmansyah <randykelvin26@gmail.com>
2022-01-12 15:53:31 +0700

    - Membuat view login - Mengimport link icon bootsreap - Memasukan Css - Membuat Layout Login dan Register

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QUERY.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QUERY.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Masuk(User parameter)
+        {
+            return Ok(parameter);
+        }
+    }
+}
