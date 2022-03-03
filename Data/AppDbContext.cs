eff32f2157b3fa2c55a8fe3f3b6bccdf1f115248
Dadan Ramdani <dramdani159@gmail.com>
2022-03-04 06:44:39 +0700

    - auto migrate di AppDbContext.cs - abaikan migrations

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -16,5 +16,48 @@ namespace QUERY.Data
         public virtual DbSet<Blog> Tb_Blog { get; set; }
         public virtual DbSet<User> Tb_User { get; set; }
         public virtual DbSet<Roles> Tb_Roles { get; set; }
+
+
+        // migrasi auto isi value ke database
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // isi tabel roles
+            modelBuilder.Entity<Roles>().HasData(new Roles[]
+            {
+                new Roles
+                {
+                    Id = "1",
+                    Name = "Admin"
+                },
+                new Roles
+                {
+                    Id = "2",
+                    Name ="User"
+                }
+            });
+
+            // isi tabel user
+            modelBuilder.Entity<User>().HasData(new User
+            {
+                Username = "randi",
+                Password = "randi",
+                Name = "Randi Firmansyah",
+                Email = "hehehe@gmail.com",
+                Roles = null
+            });
+
+            modelBuilder.Entity<Blog>().HasData(new Blog
+            {
+                Id = "1",
+                Title = "judulnya",
+                Content = "kontennya",
+                CreateDate = DateTime.Now,
+                Image = "https://avatars.githubusercontent.com/u/87772215?v=4",
+                Status = true,
+                User = null // cape ngisi jadi di null wkwk
+            });
+        }
     }
 }
---
+++ Migrations/20220303234058_automigrate.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace QUERY.Migrations
+{
+    public partial class automigrate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Tb_Blog",
+                columns: new[] { "Id", "Content", "CreateDate", "Image", "Status", "Title", "Username" },
+                values: new object[] { "1", "kontennya", new DateTime(2022, 3, 4, 6, 40, 58, 205, DateTimeKind.Local).AddTicks(7654), "https://avatars.githubusercontent.com/u/87772215?v=4", true, "judulnya", null });
+
+            migrationBuilder.InsertData(
+                table: "Tb_Roles",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { "1", "Admin" },
+                    { "2", "User" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Tb_User",
+                columns: new[] { "Username", "Email", "Name", "Password", "RolesId" },
+                values: new object[] { "randi", "hehehe@gmail.com", "Randi Firmansyah", "randi", null });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Tb_Blog",
+                keyColumn: "Id",
+                keyValue: "1");
+
+            migrationBuilder.DeleteData(
+                table: "Tb_Roles",
+                keyColumn: "Id",
+                keyValue: "1");
+
+            migrationBuilder.DeleteData(
+                table: "Tb_Roles",
+                keyColumn: "Id",
+                keyValue: "2");
+
+            migrationBuilder.DeleteData(
+                table: "Tb_User",
+                keyColumn: "Username",
+                keyValue: "randi");
+        }
+    }
+}
