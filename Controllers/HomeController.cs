5060e673fe354d4f769f181c4e53cab836d0425a
Dadan Ramdani <dramdani159@gmail.com>
2022-03-04 17:14:56 +0700

    - add postman env - fix mainhomecontroller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -14,15 +14,13 @@ namespace QUERY.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-        private readonly AppDbContext _context;
-
-        public HomeController(ILogger<HomeController> logger, AppDbContext context)
+        
+        public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
-            _context = context;
         }
 
-        // return view UntukValidasi
+        // return view UntukValidasi dengan arrow function
         public IActionResult UntukFormValidasi() => View();
 
         [HttpPost]
@@ -34,34 +32,12 @@ namespace QUERY.Controllers
 
         public IActionResult Index()
         {
-            var cekRoles = _context.Tb_Roles.ToList();
-
-            if (cekRoles.Count < 2)
-            {
-                var Tambah = new Roles[]{
-                    new Roles { Id = "1", Name = "Admin" },
-                    new Roles { Id = "2", Name = "User" }
-                };
-
-                foreach (var item in Tambah)
-                {
-                    _context.Add(item);
-                }
-                _context.SaveChangesAsync();
-            }
-
             return View();
         }
 
         public IActionResult Privacy()
         {
             return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
         public string CekValidasi([RegularExpression(@"^[0-9]*$")] string Cek)
